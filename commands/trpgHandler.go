3d763d64f5a976013c027cbf5b68ea767b1c18d5
u-ta-akane <ccn60055@protonmail.ch>
2026-06-14 18:53:15 +0900

    make trpg

--- commands/trpgHandler.go
+++ commands/trpgHandler.go
@@ -10,13 +10,21 @@ import (
 func trpgHandler(s *discordgo.Session, i *discordgo.InteractionCreate, cmds *[refs.NumberOfCommands]Command, index uint8) {
 	switch index {
 	case refs.IndexTrpgStart:
-		response := (cmds[refs.IndexTrpgSetMute]).Execute(s, i)
-	case refs.IndexTrpgSetMute:
-		result, e := utils.HasAuthority(s, i, refs.AuthorityVoiceChannelManagement)
-		response := "Authorization Error"
-		if e == 0 && result {
-			response = (cmds[refs.IndexTrpgSetMute]).Execute(s, i)
+
+		response := (cmds[refs.IndexTrpgStart]).Execute(s, i)
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
+			Data: &discordgo.InteractionResponseData{
+				Content: response,
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			utils.Log(err, "", "adminHandler")
+			return
 		}
+	case refs.IndexTrpgSetMute:
+		response := (cmds[refs.IndexTrpgSetMute]).Execute(s, i)
 		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
 			Data: &discordgo.InteractionResponseData{
@@ -29,6 +37,5 @@ func trpgHandler(s *discordgo.Session, i *discordgo.InteractionCreate, cmds *[re
 			return
 		}
 		break
-
 	}
 }
