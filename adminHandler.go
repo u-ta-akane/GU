4d535d03e880277425b6d8a4ff1908510a6a791e
prome17 <jh72179m@jh.toho-u.com>
2026-07-01 20:34:50 +0900

    why a-send-role-entrance doesnt work

--- adminHandler.go
+++ adminHandler.go
@@ -11,94 +11,48 @@ func AdminHandler(s *discordgo.Session, i *discordgo.InteractionCreate, cmds *[r
 	if i.Type != discordgo.InteractionApplicationCommand {
 		return
 	}
+	response := ""
 	switch index {
 	case refs.IndexAdminTestMessage:
 		result, e := utils.HasAuthority(s, i, refs.AuthoritySendAdminMessage)
-		response := "Authorization Error"
+		response = "Authorization Error"
 		if e == 0 && result {
 			response = (cmds[refs.IndexAdminTestMessage]).Execute(s, i)
 		}
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
-			Data: &discordgo.InteractionResponseData{
-				Content: response,
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			utils.Log(err, "", "adminHandler")
-			return
-		}
-		break
 	case refs.IndexAdminDeleteMessages:
 		result, e := utils.HasAuthority(s, i, refs.AuthorityControlMessages)
-		response := "Authorization Error"
+		response = "Authorization Error"
 		if e == 0 && result {
 			response = (cmds[refs.IndexAdminDeleteMessages]).Execute(s, i)
 		}
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
-			Data: &discordgo.InteractionResponseData{
-				Content: response,
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			utils.Log(err, "", "adminHandler")
-			return
-		}
-		break
 	case refs.IndexAdminStopBot:
 		result, e := utils.HasAuthority(s, i, refs.AuthorityBotManagement)
-		response := "Authorization Error"
+		response = "Authorization Error"
 		if e == 0 && result {
 			response = (cmds[refs.IndexAdminStopBot]).Execute(s, i)
 		}
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
-			Data: &discordgo.InteractionResponseData{
-				Content: response,
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			utils.Log(err, "", "adminHandler")
-			return
-		}
-		break
 	case refs.IndexAdminReflashRoleData:
 		result, e := utils.HasAuthority(s, i, refs.AuthorityReflashData)
-		response := "Authorization Error"
+		response = "Authorization Error"
 		if e == 0 && result {
 			response = (cmds[refs.IndexAdminReflashRoleData]).Execute(s, i)
 		}
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
-			Data: &discordgo.InteractionResponseData{
-				Content: response,
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			utils.Log(err, "", "adminHandler")
-			return
-		}
 	case refs.IndexAdminSendRoleEntranceMessage:
 		result, e := utils.HasAuthority(s, i, refs.AuthorityRoleEntranceManagement)
-		response := "Authorization Error"
+		response = "Authorization Error"
 		if e == 0 && result {
 			response = (cmds[refs.IndexAdminSendRoleEntranceMessage]).Execute(s, i)
 		}
-		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
-			Data: &discordgo.InteractionResponseData{
-				Content: response,
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			utils.Log(err, "", "adminHandler")
-			return
-		}
+	}
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource, // 「通常の返答」タイプ
+		Data: &discordgo.InteractionResponseData{
+			Content: response,
+			Flags:   discordgo.MessageFlagsEphemeral,
+		},
+	})
+	if err != nil {
+		utils.Log(err, "", "adminHandler")
+		return
 	}
 }
